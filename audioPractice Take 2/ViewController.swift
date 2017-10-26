18e3d42dc480b269ea6e8622186dde402860e84d
Andrew James Kinsey <akinsey@Andrews-MacBook-Air-123.local>
2017-10-26 10:44:33 -0500

    Main Project Done

--- audioPractice Take 2/ViewController.swift
+++ audioPractice Take 2/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func playSong(song: String) {
+        let path = Bundle.main.path(forResource: "\(song).mp3", ofType: nil)!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+        }
+        catch {
+            let alert = UIAlertController(title: "The song title has not been found.", message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+        player?.play()
     }
-
-
+    
+    @IBAction func playSong2(_ sender: Any) {
+        playSong(song: "marbles")
+    }
+    
+    @IBAction func playSong(_ sender: Any) {
+        playSong(song: "mozartSong")
+    }
+    
+    @IBAction func pauseSong(_ sender: Any) {
+        player?.pause()
+    }
 }
 
